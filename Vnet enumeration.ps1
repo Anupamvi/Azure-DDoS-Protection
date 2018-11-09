046264a626d98bcacd2a302d928556a08c44e5df
Anupam Vij <31665335+Anupamvi@users.noreply.github.com>
2018-11-08 17:17:48 -0800

    Add files via upload

---
+++ Vnet enumeration.ps1
@@ -0,0 +1,45 @@
+#Sign in to azure account
+Connect-AzureRmAccount
+
+$vnet_attrs = @()
+
+#get all subscriptions
+$subs = Get-AzureRmSubscription 
+foreach ($Sub in $Subs) { 
+    #select subscription
+    $SelectSub = Select-AzureRmSubscription -SubscriptionName $Sub.Name 
+    #get VNETs in subscription
+    $VNETs = Get-AzureRmVirtualNetwork -WarningAction SilentlyContinue
+
+    Write-Host "Checking Vnets on subscription: " -NoNewLine; Write-Host "$($Sub.Name) " -ForegroundColor Cyan;
+    foreach ($vnet in $VNETs) {
+        $subnets = $vnet.Subnets
+        $ipconfigs = $subnets.IpConfigurations
+
+        #Dont log information for VNETs that have 0 ip configs behind them
+        if ($ipconfigs.Length -eq 0){
+            continue;
+        } 
+
+        #get the attributes of this vnet and append it to our list of vnets
+        $attr = New-Object -TypeName PSObject
+        Add-Member -InputObject $attr -MemberType NoteProperty -Name "VnetName" -Value $vnet.Name
+        Add-Member -InputObject $attr -MemberType NoteProperty -Name "ResourceGroup" -Value $vnet.ResourceGroupName
+        Add-Member -InputObject $attr -MemberType NoteProperty -Name "Subscription" -Value $Sub.Name
+        Add-Member -InputObject $attr -MemberType NoteProperty -Name "NumberOfIPs" -Value $ipconfigs.Length
+        Add-Member -InputObject $attr -MemberType NoteProperty -Name "DDoSEnabled" -Value $vnet.EnableDdosProtection
+        $vnet_attrs += $attr
+    } 
+} 
+
+
+#DDoS is enabled
+Write-Host "The following VNets do have DDoS Enabled: " -ForegroundColor Green
+$vnet_attrs | Group-Object -Property DDoSEnabled | ? { $_.Name -eq 'True'} | ForEach-Object { $_.Group | Sort-Object -Property NumberOfIPs -Descending | Format-Table }
+
+#DDoS is disabled
+Write-Host "The following VNets do not have DDoS Enabled: " -ForegroundColor Red
+$vnet_attrs | Group-Object -Property DDoSEnabled | ? { $_.Name -eq 'False'} | ForEach-Object { $_.Group | Sort-Object -Property NumberOfIPs -Descending | Format-Table }
+
+#Write list to CSV of currently logged in users desktop. Replace your drive C:\ with the drive that contains your user information
+$vnet_attrs | Sort-Object -Property NumberOfIPs, DDoSEnabled -Descending | Export-Csv -Path c:\users\$env:USERNAME\desktop\AzureVnet_DDoSInformation.csv -NoTypeInformation 
